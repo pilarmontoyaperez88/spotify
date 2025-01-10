da1de07940459eefe581c9fd2eb6a1ef89be460c
pilarmontoyaperez88 <pilar.montoyaperez@plexus.es>
2025-01-10 11:17:00 +0100

    Se creo la vista globalView, para mostar las playlist. tambien se creo una composable

---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { redirectToAuthCodeFlow } from '@/service/authService'
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+
+function redirectToAuth() {
+  if (clientId) {
+    redirectToAuthCodeFlow(clientId)
+  } else {
+    console.error('clientId is not defined!')
+  }
+}
+</script>
+
+<template>
+  <div class="home">
+    <h1>Bienvenido a tu app de Spotify</h1>
+    <p>Haz click en el botón para ver tu perfil de Spotify.</p>
+    <button @click="redirectToAuth">Login con Spotify</button>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: #1db954; /* Spotify Green */
+  color: white;
+  border: none;
+  border-radius: 50px;
+}
+
+button:hover {
+  background-color: #1ed760;
+}
+</style>
